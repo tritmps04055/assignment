79216b86734b5c70399f78c648c22bbba0f020cf
Trương Minh Trí <Trng Minh Tr>
2017-04-16 20:44:08 +0700

    First commit

---
+++ libraries/config.php
@@ -0,0 +1,9 @@
+<?php
+	class config{
+		const HOST_NAME = "mysql.hostinger.vn";
+		const HOST_USER = "u535631408_user";
+		const HOST_PASS = "minhtri";
+		const DB_NAME	= "u535631408_db";
+	}
+	define("base_url","http://www.assignmentbanhang.hol.es/public_html/");
+?>
